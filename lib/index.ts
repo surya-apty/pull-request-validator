93bc0ffa01bc026834702800d41e8e335594e486
surya <sing.shekhawat67@gmail.com>
2022-10-13 11:22:40 +0530

    update

--- lib/index.ts
+++ lib/index.ts
@@ -1,25 +1,8 @@
 import { getLocalBranch } from "./git-commands";
-import inquirer from "inquirer";
 
 getLocalBranch().then((branch) => {
   console.log(branch);
-  inquirer
-    .prompt([
-      'Do you want to create pull request?',
-      'Not Sure?'
-    ])
-    .then((answers) => {
-      // Use user feedback for... whatever!!
-      console.log(answers);
-      
-    })
-    .catch((error: { isTtyError: any; }) => {
-      if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-      } else {
-        // Something else went wrong
-      }
-    });
+  
 });
 const branchValidatorRegex = new RegExp(/\[\s(CB|LSP)-\d+\s\]/g);
 console.log(
