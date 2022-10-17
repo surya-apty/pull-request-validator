b16b31cf06ef17b4a5d24228e5367f220cf62bde
Surya <85492089+surya-apty@users.noreply.github.com>
2022-10-17 10:49:07 +0530

    added cli and removed pkg

--- dist/types/cli.d.ts
+++ dist/types/cli.d.ts
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+declare const prompts: any;
+declare const questions: ({
+    type: string;
+    name: string;
+    message: string;
+    initial?: undefined;
+} | {
+    type: string;
+    name: string;
+    message: string;
+    initial: string;
+})[];
--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-console.log('hello surya');
+const prompts = require('prompts');
+
+const questions = [
+    {
+      type: 'text',
+      name: 'username',
+      message: 'What is your GitHub username?'
+    },
+    {
+      type: 'number',
+      name: 'age',
+      message: 'How old are you?'
+    },
+    {
+      type: 'text',
+      name: 'about',
+      message: 'Tell something about yourself',
+      initial: 'Why should I?'
+    }
+  ];
+  
+  (async () => {
+    const response = await prompts(questions);
+    console.log(response);
+    
+    // => response => { username, age, about }
+  })();
